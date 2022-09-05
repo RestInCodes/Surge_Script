371105f5d9cd923504c3fc4888bc58ce2add3cec
RestInCodes <32709576+RestInCodes@users.noreply.github.com>
2022-09-05 20:16:02 +0800

    Update net_info.js

--- net_info.js
+++ net_info.js
@@ -46,7 +46,7 @@ const v4IP = v4.primaryAddress;
           `IP：${ip} \n`+ 
           (wifi.ssid ? `路由器地址：${router}\n` : "")+
           `节点: rootName\n`+
-          `外部 IP：${externalIP}\n`+
+          `外部-IP：${externalIP}\n`+
 	  `国家地区: ${jsonData.country} - ${jsonData.city}\n`+
           `运营商: ${jsonData.isp}\n` +
 	  `数据中心: ${jsonData.org}`,
